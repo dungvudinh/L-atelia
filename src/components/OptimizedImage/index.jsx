3a846a69a482795127aef5241453c31d14038db6
unknown <anhkho881@gmail.com>
2025-10-28 14:59:40 +0700

    refactor OptimizedImage component

--- src/components/OptimizedImage/index.jsx
+++ src/components/OptimizedImage/index.jsx
@@ -1,19 +1,14 @@
 const OptimizedImage = ({ src, alt, className, ...props }) => {
-  const originalSrc = src;
-  const webpSrc = src.replace(/\.(jpg|jpeg|png)$/, '.webp');
-  
   return (
-    <picture>
-      <source srcSet={webpSrc} type="image/webp" />
-      <source srcSet={originalSrc} type="image/jpeg" />
-      <img 
-        src={originalSrc} 
-        alt={alt} 
-        className={className}
-        loading="lazy"
-        {...props}
-      />
-    </picture>
+    <img 
+      src={src} 
+      alt={alt} 
+      className={className}
+      loading="lazy"
+      decoding="async"
+      {...props}
+    />
   );
 };
+
 export default OptimizedImage;
