e82cf73a295009c26186836490d3ebf624205552
unknown <anhkho881@gmail.com>
2025-11-17 10:36:43 +0700

    change API URL

--- src/layouts/components/Header/index.jsx
+++ src/layouts/components/Header/index.jsx
@@ -7,7 +7,7 @@ import {setLanguage} from '../../../redux/actions/languageActions';
 import {LocalizedLink} from '../../../components/LocalizedLink';
 const MENU_ITEMS = [
     {id:1, title:'projects', to:'/projects'},
-    {id:2, title:'properties for sale', to:'/properties-for-sale'}, 
+    // {id:2, title:'properties for sale', to:'/properties-for-sale'}, 
     {id:3, title:'properties for rent', to:'/properties-for-rent'}, 
     {id:4, title:'about us', to:'/about'}, 
     {id:5, title:'media', to:'/media'}, 
