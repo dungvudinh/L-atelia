77a6e0a2a5ad07e66d8c2e2ea936d69761f76a27
unknown <anhkho881@gmail.com>
2026-03-19 09:49:18 +0700

    using cdn for media featuredImage

--- src/pages/MediaDetail/index.jsx
+++ src/pages/MediaDetail/index.jsx
@@ -91,8 +91,8 @@ function MediaDetail() {
     // Get media image - fallback to default image
     const getMediaImage = (mediaItem) => {
         if (mediaItem?.featuredImage) 
-            // return 'https://cdn.latelia.com/latelia/' + mediaItem.featuredImage.key;
-            return mediaItem.featuredImage.url;
+            return 'https://cdn.latelia.com/latelia/' + mediaItem.featuredImage.key;
+            // return mediaItem.featuredImage.url;
         return media1;
     };
 
